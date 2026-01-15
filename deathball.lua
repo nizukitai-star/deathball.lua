8ebaef5d5e4ac043892f0b2ffaf57a9b7c1e982d
Artem <nizukitai@gmail.com>
2026-01-15 15:07:34 -0300

    Create deathball.lua

---
+++ deathball.lua
@@ -0,0 +1,9 @@
+-- Artem I - Death Ball
+
+game.StarterGui:SetCore("SendNotification", {
+    Title = "Artem I - Death Ball",
+    Text = "Script funcionando!",
+    Duration = 5
+})
+
+print("Artem I - Death Ball carregado com sucesso!")
